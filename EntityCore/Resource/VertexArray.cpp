e5242d5740e8a77512a41c3861f4669b6eca56e8
Calvin Ruiz <calvin.ruiz@epitech.eu>
2021-06-17 10:55:38 +0200

    Implement VertexArray

--- EntityCore/Resource/VertexArray.cpp
+++ EntityCore/Resource/VertexArray.cpp
@@ -1,26 +1,65 @@
 #include "EntityCore/Core/VulkanMgr.hpp"
-#include "VertexBuffer.hpp"
 #include "VertexArray.hpp"
+#include "VertexBuffer.hpp"
 
 static int getFormatSize(VkFormat format)
 {
     switch (format) {
         case VK_FORMAT_R32_SFLOAT:
         case VK_FORMAT_R32_SINT:
-        case VK_FORMAT_R32_UINT: return 1;
+        case VK_FORMAT_R32_UINT: return 4;
         case VK_FORMAT_R32G32_SFLOAT:
         case VK_FORMAT_R32G32_SINT:
-        case VK_FORMAT_R32G32_UINT: return 2;
+        case VK_FORMAT_R32G32_UINT: return 8;
         case VK_FORMAT_R32G32B32_SFLOAT:
         case VK_FORMAT_R32G32B32_SINT:
-        case VK_FORMAT_R32G32B32_UINT: return 3;
+        case VK_FORMAT_R32G32B32_UINT: return 12;
         case VK_FORMAT_R32G32B32A32_SFLOAT:
         case VK_FORMAT_R32G32B32A32_SINT:
-        case VK_FORMAT_R32G32B32A32_UINT: return 4;
+        case VK_FORMAT_R32G32B32A32_UINT: return 16;
         default: assert(false); return 0;
     }
 }
 
-VertexArray::VertexArray(VulkanMgr &master) : master(master)
+VertexArray::VertexArray(VulkanMgr &master, int alignment) : master(master), alignment(alignment)
 {
+}
+
+bool VertexArray::createBindingEntry(uint32_t stride, VkVertexInputRate type)
+{
+    if (alignment % stride) {
+        master.putLog("Alignment with value " + std::to_string(alignment) + " must be a multiple of stride with value " + std::to_string(stride), LogType::WARNING);
+        return false;
+    }
+    bindingDesc.push_back({binding++, stride, type});
+    size = stride;
+    location = 0;
+    offset = 0;
+    return true;
+}
+
+bool VertexArray::addInput(VkFormat format, bool used)
+{
+    const int subSize = getFormatSize(format);
+    if (size < subSize) {
+        master.putLog("Unsufficient stride for binding " + std::to_string(binding), LogType::WARNING);
+        return false;
+    }
+    size -= subSize;
+    if (used)
+        attributeDesc.push_back({location++, binding, format, offset});
+    offset += size;
+    return true;
+}
+
+VertexBuffer *VertexArray::createBuffer(int binding, int vertexCount, BufferMgr *vertexMgr, BufferMgr *instanceMgr)
+{
+    switch (bindingDesc[binding].inputRate) {
+        case VK_VERTEX_INPUT_RATE_VERTEX:
+            return new VertexBuffer(*vertexMgr, vertexCount, bindingDesc[binding].stride, alignment);
+        case VK_VERTEX_INPUT_RATE_INSTANCE:
+            return new VertexBuffer(*instanceMgr, vertexCount, bindingDesc[binding].stride, alignment);
+        default:
+            return nullptr;
+    }
 }
--- EntityCore/Resource/VertexArray.hpp
+++ EntityCore/Resource/VertexArray.hpp
@@ -9,6 +9,7 @@
 
 class VulkanMgr;
 class VertexBuffer;
+class BufferMgr;
 
 /*
 * A VertexBuffer layout to facilitate binding in pipeline
@@ -16,10 +17,14 @@ class VertexBuffer;
 class VertexArray
 {
 public:
-    VertexArray(VulkanMgr &master);
+    VertexArray(VulkanMgr &master, int alignment);
     ~VertexArray() = default;
 
-    
+    // Create a binding entry which match a VertexBuffer binding
+    bool createBindingEntry(uint32_t stride, VkVertexInputRate type);
+    // Add input for the last binding entry created
+    bool addInput(VkFormat format, bool used = true);
+    VertexBuffer *createBuffer(int binding, int vertexCount, BufferMgr *vertexMgr = nullptr, BufferMgr *instanceMgr = nullptr);
     //! Internally used
     const std::vector<VkVertexInputBindingDescription> &getVertexBindingDesc() {return bindingDesc;}
     //! Internally used
@@ -28,6 +33,11 @@ private:
     VulkanMgr &master;
     std::vector<VkVertexInputBindingDescription> bindingDesc;
     std::vector<VkVertexInputAttributeDescription> attributeDesc;
+    uint32_t binding = 0;
+    int size = 0;
+    uint32_t location = 0;
+    uint32_t offset = 0;
+    const int alignment;
 };
 
 #endif /* end of include guard: VERTEX_ARRAY_HPP */
