015e54de060671e571afb1d30a2936cff8137404
Calvin-Ruiz <43058983+Calvin-Ruiz@users.noreply.github.com>
2021-10-15 10:05:15 +0200

    Add Forward.hpp (to forward-define everything)

---
+++ EntityCore/Forward.hpp
@@ -0,0 +1,18 @@
+// Regroup almost every forward declarations
+
+class VulkanMgr;
+class BufferMgr;
+class FrameMgr;
+class RenderMgr;
+
+class Pipeline;
+class ComputePipeline;
+class PipelineLayout;
+class SetMgr;
+class Set;
+class SyncEvent;
+class Texture;
+class VertexArray;
+class VertexBuffer;
+class TransferMgr;
+template <typename T> SharedBuffer;
